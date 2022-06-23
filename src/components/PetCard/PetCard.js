701504afeaeb0d82fa9b6a02be10ac2d234eb0c0
Cristina Pazmiño <cristinapazmino59@gmail.com>
2022-06-23 15:51:25 -0500

    Home con slide

--- src/components/PetCard/PetCard.js
+++ src/components/PetCard/PetCard.js
@@ -1,9 +1,14 @@
-import React from "react"
+import React from "react";
+import { Modal } from "bootstrap";
 import {Link} from "react-router-dom";
 import s from "./PetCard.module.css";
 import capitalizeFirst from "../../hooks/capitalizeFirst";
 
 const PetCard = ({ pet }) => {
+
+    const addToFav = (e) => {
+        e.preventDefault();
+    }
     
     return (
         <li className={s.petCard}>
@@ -12,10 +17,11 @@ const PetCard = ({ pet }) => {
             </Link>
             <div className={s.petInfo}>
                 <span className={s.petName}>{capitalizeFirst(pet.name)}</span><br/>
-                <span>{capitalizeFirst(pet.age)}</span><br/>
-                <span>{capitalizeFirst(pet.gender)}</span><br/>
+                <span>{`${capitalizeFirst(pet.age)}, ${capitalizeFirst(pet.gender)}`}</span><br/>
+{/*                 <span>{capitalizeFirst(pet.gender)}</span><br/> */}
                 <span>{capitalizeFirst(pet.location)}</span>
             </div>
+            <button className={s.favButton} type="submit" onClick={addToFav} > Add to favorites</button>
         </li>
     )
 }
