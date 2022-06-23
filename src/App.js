8bfd39f0e99035b6712bd565d5bfb307f0a70898
gastulo <gastonchemi@gmail.com>
2022-06-24 00:11:58 +0300

    single page application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import Home from "./components/Home/Home"
+import SinglePetCard from "./components/SinglePetCard/SinglePetCard"
 
 const App = () => {
   return (
-    <div>Hola mundo!
+    <div>
+      Hola mundo!
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/animals/:id" element={<SinglePetCard />}></Route>
       </Routes>
     </div>
   )
--- src/components/PetCard/PetCard.js
+++ src/components/PetCard/PetCard.js
@@ -4,10 +4,9 @@ import s from "./PetCard.module.css";
 import capitalizeFirst from "../../hooks/capitalizeFirst";
 
 const PetCard = ({ pet }) => {
-    
     return (
         <li className={s.petCard}>
-            <Link to={`/${pet.id}` }>
+            <Link to={`/animals/${pet.id}`} propiedades={'Hello'}>
                 <img className={s.petImage} src={require(`../../assets/img${pet.url_path1}`)} alt={pet.name}></img>
             </Link>
             <div className={s.petInfo}>
