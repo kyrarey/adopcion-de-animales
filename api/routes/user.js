98fee3a8a039ea5b5356d63d356bc4b80b88f969
Fabricio Rossi <frprogra16@gmail.com>
2022-06-28 10:39:54 -0300

    jwt

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,25 +2,51 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const db = require("../db")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
-//registrar usuario nuevo
-router.post("/register", (req, res) => {
-  User.create(req.body)
-    .then((user) => {
-      res.status(201).send(user);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
+// Register
+router.post('/register', async (req, res) => {
+  console.log(req.body, '            register')
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
+    const newUser = {
+      ...req.body,
+      password,
+    };
+    const user = await User.create(newUser);
+    res.status(201).send(user);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+  const user = await User.findOne({user:req.body});
+  const passwordIsCorrect =
+    user === null
+      ? false
+      : await bcrypt.compare(req.body.password, user.password);
+  if ((user && passwordIsCorrect)) {
+    return res.status(401).json({
+      error: 'invalid user or password',
     });
+  }
+  const userForToken = {
+    id: user.id,
+    mail: user.mail,
+  };
+  const token = jwt.sign(userForToken, "organizacion.messi");
+  console.log(user)
+  res.send({
+    ...user.dataValues,
+    token,
+  });
 });
 
-//login usuario con JWT
-router.post("/login", (req,res) =>{
-  User.find(req.user.data)
-  .then((user) => { 
-    res.status(201).send(user)
-  })
-});
+
 
 //editar usuario
 router.put("/:id", (req, res) => {
